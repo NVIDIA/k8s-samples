6398d9e90109a843321bd96c4eee55961d556b32
Christopher Desiniotis <cdesiniotis@nvidia.com>
2022-11-04 11:57:31 -0700

    Bump cuda samples to v11.8 and cuda base image to v11.8.0
    Signed-off-by: Christopher Desiniotis <cdesiniotis@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 # Specify the CUDA base image version
-CUDA_VERSION ?= 11.7.1
+CUDA_VERSION ?= 11.8.0
 VERSION ?= cuda${CUDA_VERSION:cuda%=%}
 
 # Specify the tag for the https://github.com/NVIDIA/cuda-samples repository.
 # This need not match the CUDA_VERSION above.
-CUDA_SAMPLES_VERSION := v11.6
+CUDA_SAMPLES_VERSION := v11.8
