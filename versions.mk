2dbf715f2adbf366065052fea5d8516613f43814
Christopher Desiniotis <cdesiniotis@nvidia.com>
2023-05-22 12:48:15 -0700

    Bump cuda samples to v12.1
    Signed-off-by: Christopher Desiniotis <cdesiniotis@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -18,4 +18,4 @@ VERSION ?= cuda${CUDA_VERSION:cuda%=%}
 
 # Specify the tag for the https://github.com/NVIDIA/cuda-samples repository.
 # This need not match the CUDA_VERSION above.
-CUDA_SAMPLES_VERSION := v11.6
+CUDA_SAMPLES_VERSION := v12.1
